1f74cb27fae0c360428a4c0efb7e590a4c5e7fc9
Anrric <sanchezponce315@gmail.com>
2024-09-07 21:40:14 -0500

    formulario doctor

---
+++ server/client/src/doctorView.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import './doctorView.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+//components
+import InputDoctor from './components/inputDoctor/InputDoctor';
+import Header from './components/header/header';
+function doctorView() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <InputDoctor />
+    </div>
+  );
+}
+
+export default doctorView;
