a4a0b92aa60d1496658036d2a1a42069637b0a6a
not-dotcom <juliogarcu11@gmail.com>
2024-12-01 22:10:41 -0600

    css

--- server/client/src/landing/carousel/carousel.js
+++ server/client/src/landing/carousel/carousel.js
@@ -1,21 +1,31 @@
-'use client';
-import React from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
-import './carousel.css'
-import Autoplay from 'embla-carousel-autoplay'
-import Test from '../motion/test';
+"use client";
+import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import "./carousel.css";
+import Autoplay from "embla-carousel-autoplay";
+import Test from "../motion/test";
 import imageFlayer from "../../media/flayer.png";
 export function EmblaCarousel() {
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 700000 })])
-
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [
+    Autoplay({ delay: 700000 }),
+  ]);
 
   return (
     <div className="embla" ref={emblaRef}>
       <div className="embla__container">
-        <div className="embla__slide"><Test></Test></div>
-        <div className="embla__slide"><img src={imageFlayer} width="900px" height="900px" className="imageFlayer" alt="Profile Image"></img></div>
+        <div className="embla__slide">
+          <Test></Test>
+        </div>
+     
+        <div className="embla__slide">
+          <img
+            src={imageFlayer}
+         
+            className="imageFlayer"
+            alt="Profile Image"
+          ></img>
+        </div>
       </div>
-
     </div>
-  )
+  );
 }
